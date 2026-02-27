b8ccdb9e06ceaf525a7b470ffe1dced4fe8ab328
TadiwaMakoni05 <tadiwa.makoni13@gmail.com>
2026-02-27 14:07:05 +0200

    z index fix

--- components/projects.tsx
+++ components/projects.tsx
@@ -130,7 +130,7 @@ export function Projects() {
 
       <AnimatePresence>
         {selectedProject && (
-          <div className="fixed inset-0 z-[100] flex items-center justify-center p-4 sm:p-6 md:p-12">
+          <div className="fixed inset-0 z-100 flex items-center justify-center p-4 sm:p-6 md:p-12">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
